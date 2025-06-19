b8b95a5c1532df92d62289cbb1bbb51f3d7e291a
Raniro Coelho <ranirocoelho@gmail.com>
2025-06-18 21:07:13 -0300

    add UI for field creation with type, required and options

--- app.py
+++ app.py
@@ -25,3 +25,35 @@ if selected_file:
 st.subheader("Form Metadata")
 form_name = st.text_input("Form Name", form_data.get("form_name", ""))
 issue_type = st.text_input("Issue Type", form_data.get("issue_type", ""))
+
+# ---- Fields Management ----
+st.subheader("Fields")
+
+if "fields" not in form_data or not isinstance(form_data.get("fields"), list):
+    form_data["fields"] = []
+
+with st.form("add_field_form", clear_on_submit=True):
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        field_name = st.text_input("Field Name")
+    with col2:
+        field_type = st.selectbox(
+            "Field Type",
+            [
+                "text", "textarea", "select", "number", "date",
+                "checkbox", "radio", "userpicker", "projectpicker",
+            ],
+        )
+    with col3:
+        required = st.checkbox("Required", value=False)
+
+    options = []
+    if field_type in ["select", "checkbox", "radio"]:
+        options = [
+            opt.strip()
+            for opt in st.text_input("Options (comma separated)").split(",")
+            if opt.strip()
+        ]
+
+    submitted = st.form_submit_button("Add Field")
